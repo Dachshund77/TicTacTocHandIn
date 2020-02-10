51ffb87ab1e83b32dbf6ac6421d073fe919523f4
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-02-11 00:22:41 +0100

    LAst Commit

--- TicTacTocHandIn/TicTacTocHandIn/App.xaml.cs
+++ TicTacTocHandIn/TicTacTocHandIn/App.xaml.cs
@@ -10,7 +10,14 @@ namespace TicTacTocHandIn
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainMenuPage());
+            //Setting Style in code behinde for the navigationBar
+            NavigationPage navigationPage = new NavigationPage(new MainMenuPage());
+
+            Style navStyle = Resources["NavigationPageStyle"] as Style;
+
+            navigationPage.Style = navStyle;
+
+            MainPage = navigationPage;
         }
 
         protected override void OnStart()
--- TicTacTocHandIn/TicTacTocHandIn/SimpleGame.xaml.cs
+++ TicTacTocHandIn/TicTacTocHandIn/SimpleGame.xaml.cs
@@ -12,7 +12,7 @@ namespace TicTacTocHandIn
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SimpleGame : ContentPage
     {
-        private Players curPlayerTurn;  
+        private Players curPlayerTurn;
 
         public Players CurPlayerTurn
         {
@@ -27,20 +27,37 @@ namespace TicTacTocHandIn
             }
         }
 
+        public Players?[,] GameBoard;
+
         public SimpleGame()
         {
             InitializeComponent();
+            BindingContext = this;
             this.CurPlayerTurn = DeterminStartPlayer();
+            this.GameBoard = new Players?[3, 3];
         }
 
-        private void TakePlayerTurn()
+        private async void TakePlayerTurn(Players player, int column, int row, Button button)
         {
-            if (HasGameEnded(CurPlayerTurn))
+            //Test if valid move
+            if (GameBoard[column, row] != null)
             {
-                SendEndGameMessage(CurPlayerTurn);
-            } else
+                return; // Abot call 
+            }
+
+            //Update game board
+            GameBoard[column, row] = player;
+
+            //Update button
+            MarkGameButton(player, button);
+
+            if (HasGameEnded(player))
             {
-                switch (CurPlayerTurn)
+                await SendEndGameMessageAsync(player); //Ignore this warning
+            }
+            else
+            {
+                switch (player)
                 {
                     case Players.Cross:
                         CurPlayerTurn = Players.Circle;
@@ -49,7 +66,7 @@ namespace TicTacTocHandIn
                         CurPlayerTurn = Players.Cross;
                         break;
                     default:
-                        throw new Exception();                      
+                        throw new Exception();
                 }
             }
         }
@@ -58,7 +75,7 @@ namespace TicTacTocHandIn
         {
             Random rand = new Random();
             int randomInt = rand.Next(2);
-            if(randomInt == 0)
+            if (randomInt == 0)
             {
                 return Players.Cross;
             }
@@ -70,22 +87,140 @@ namespace TicTacTocHandIn
 
         private bool HasGameEnded(Players p)
         {
-            //3 down
-            //if(Button7 == p && )
-            //3 left
-
-            //2 from corner to corner 
-            return true;
+            //Test if given player p matched 
+            if (MatchedRow(p, 0) ||
+                MatchedRow(p, 1) ||
+                MatchedRow(p, 2) ||
+                MatchedColumn(p, 0) ||
+                MatchedColumn(p, 1) ||
+                MatchedColumn(p, 2) ||
+                MatchedDiagonally(p)
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        private void SendEndGameMessage(Players player)
+        private bool MatchedRow(Players p, int row)
         {
+            if (GameBoard[0, row] == p &&
+                GameBoard[1, row] == p &&
+                GameBoard[2, row] == p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool MatchedColumn(Players p, int column)
+        {
+            if (GameBoard[column, 0] == p &&
+                GameBoard[column, 1] == p &&
+                GameBoard[column, 2] == p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool MatchedDiagonally(Players p)
+        {
+            //top left to bottom right
+            if (GameBoard[0, 0] == p &&
+               GameBoard[1, 1] == p &&
+               GameBoard[2, 2] == p)
+            {
+                return true;
+            }
+            //top right to bottom left
+            else if (GameBoard[2, 0] == p &&
+                    GameBoard[1, 1] == p &&
+                    GameBoard[0, 2] == p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
 
         }
 
+        private void MarkGameButton(Players player, Button button) {
+            switch (player)
+            {
+                case Players.Cross:
+                    button.Text = "X";
+                    break;
+                case Players.Circle:
+                    button.Text = "O";
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
 
+        private async Task SendEndGameMessageAsync(Players player)
+        {
+            await DisplayAlert("Game over", player + " has won the game!", "OK");
+            await Navigation.PopAsync();
+        }
 
-        
+        private void Button7_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 0, 0, (Button)sender);
+        }
 
+        private void Button8_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 1, 0, (Button)sender);
+        }
+
+        private void Button9_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 2, 0, (Button)sender);
+        }
+
+        private void Button4_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 0, 1, (Button)sender);
+        }
+
+        private void Button5_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 1, 1, (Button)sender);
+        }
+
+        private void Button6_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 2, 1, (Button)sender);
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 0, 2, (Button)sender);
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 1, 2, (Button)sender);
+        }
+
+        private void Button3_Clicked(object sender, EventArgs e)
+        {
+            TakePlayerTurn(CurPlayerTurn, 2, 2, (Button)sender);
+        }
     }
 }
